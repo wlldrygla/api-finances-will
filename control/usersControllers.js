8ee081dc74dbee75b69361e14b3dc2c6e332ba31
wlldrygla <willian09.cardoso@gmail.com>
2023-07-02 21:52:51 -0300

    fix: login controller AGORA VAI

--- control/usersControllers.js
+++ control/usersControllers.js
@@ -2,28 +2,18 @@
 const usersController = function (Users) {
     const userLogin = function (req, res) {
         Users.find({ user: req.body.user }).exec(function (err, user) {
-            if(err){
-                res.send(err)
-            }
-            if( user.password === req.body.password && user.user === req.body.user){
-                res.json({ user: user[i].user })
-            }else{
+            if (err) {
                 res.send('erro')
-            }
-            // if (err) {
-            //     res.send('erro')
-            // } else if (user.length === 0) {
-            //     res.json({ erro: "Usuário Incorreto" })
-            // } else {
-            //     for (let i = 0; i < 3; i++) {
-            //         if (i > user.length) {
-            //             res.json({ erro: "Senha Incorreta" })
-            //         } else if (user[i].password ? user.password === req.body.password : undefined) {
-            //             res.json({ user: user[i].user })
-            //         }
-            //     }
-            // }
-        })
+            } 
+                if (user[0].password === req.body.password && user[0].user === req.body.user) {
+                    res.json({ user: user[i].user })
+                } else {
+                    res.json('ERRO')
+                }
+            
+        }
+
+        )
     };
 
     return {
