052df681599cf22a2be1c9c0bbcd599c634cc782
wlldrygla <willian09.cardoso@gmail.com>
2023-07-02 22:02:56 -0300

    fix: login controller AGORA VAI

--- control/usersControllers.js
+++ control/usersControllers.js
@@ -4,15 +4,19 @@ const usersController = function (Users) {
         Users.find({ user: req.body.user }).exec(function (err, user) {
             if (err) {
                 res.send('erro')
-            } 
+            }
+            if (user) {
                 if (user[0].password === req.body.password && user[0].user === req.body.user) {
                     res.json({ user: user[0].user })
                 } else {
-                    res.json('ERRO')
+                    res.status(401);
+                    res.json('SENHA INVÁLIDA')
                 }
-            
+            } else {
+                res.status(401);
+                res.json('USUÁRIO NÃO ENCONTRADO')
+            }
         }
-
         )
     };
 
