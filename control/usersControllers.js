bc8581538c6ae44abdd51b05c375319919aab535
wlldrygla <willian09.cardoso@gmail.com>
2023-07-02 21:00:46 -0300

    fix: fixing login function

--- control/usersControllers.js
+++ control/usersControllers.js
@@ -4,13 +4,17 @@ const usersController = function (Users) {
         Users.find({ usuario: req.body.user }).exec(function (err, user) {
             if (err) {
                 res.send(`erro ${err}`)
+                console.log('1')
             } else if (user.length === 0) {
                 res.json({ erro: "Usuário Incorreto" })
+                console.log('2')
             } else {
                 if (user.password === req.body.password) {
                     res.json({ user: user.user })
+                    console.log('3')
                 } else {
                     res.json({ erro: "Senha Incorreta" })
+                    console.log('4')
                 }
             }
         })
